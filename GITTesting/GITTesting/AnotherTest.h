c7da10c92eb159f26d1be7cdeb3d77336b2ce6f7
xiongsilan <xiongsilan@bytedance.com>
2018-02-23 22:48:53 +0800

    third commit

---
+++ GITTesting/GITTesting/AnotherTest.h
@@ -0,0 +1,13 @@
+//
+//  AnotherTest.h
+//  GITTesting
+//
+//  Created by WithHeart on 2018/2/23.
+//  Copyright © 2018年 WithHeart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol AnotherTest <NSObject>
+
+@end
